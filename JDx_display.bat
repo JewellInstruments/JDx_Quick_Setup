61ce05c972544e6f4c68e8cf78d4c36c558b526d
cbrochu1 <cbrochu@jewellinstruments.com>
2025-12-18 10:51:30 -0500

    Update JDx_display.bat

--- JDx_display.bat
+++ JDx_display.bat
@@ -2,4 +2,4 @@ git pull https://github.com/JewellInstruments/JDx_Quick_Setup.git main
 call ".venv\Scripts\activate.bat"
 @REM pip install -U -r requirements.txt
 python JDx_display.py
-pause
+close
